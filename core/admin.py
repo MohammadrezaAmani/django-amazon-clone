37b6afb7daa62a6c4b9b2854452299b204e00b41
Mohammadreza Amani <More.Amani@yahoo.com>
2023-01-15 04:11:12 +0330

    add review to admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order,Category
+from .models import Item, OrderItem, Order, Category, Review
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(Category)
+admin.site.register(Review)
