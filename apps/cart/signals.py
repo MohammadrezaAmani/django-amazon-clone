50e62f8d152e446f56388300de05e5165596802c
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-18 00:07:05 +0330

    Add signal to trigger admin notification on cart item added

---
+++ apps/cart/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CartItem
+from .tasks import notify_admins_on_cart_item_added
+
+
+@receiver(post_save, sender=CartItem)
+def notify_admins_on_cart_item_added_signal(sender, instance, created, **kwargs):
+    if created:
+        notify_admins_on_cart_item_added.delay(instance.id)  # type: ignore
