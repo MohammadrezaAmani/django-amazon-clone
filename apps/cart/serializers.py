16207e9987f5d91a34a65793148aa6395aae0142
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-18 00:07:01 +0330

    Create cart serializers

---
+++ apps/cart/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Cart, CartItem
+from apps.products.serializers import ProductVariantSerializer
+from apps.products.models import ProductVariant
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    variant = ProductVariantSerializer(read_only=True)
+    variant_id = serializers.PrimaryKeyRelatedField(
+        queryset=ProductVariant.objects.all(),  # type:ignore
+        source="variant",
+        write_only=True,
+    )
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "variant", "variant_id", "quantity", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = [
+            "id",
+            "user",
+            "session_id",
+            "items",
+            "created_at",
+            "updated_at",
+            "metadata",
+        ]
+        read_only_fields = ["id", "user", "created_at", "updated_at", "metadata"]
