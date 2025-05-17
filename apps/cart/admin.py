3acff5b0ce80cbcbbdd658b5c95f37200a8edf2a
Mohammadreza Amani <More.Amani@yahoo.com>
2025-05-18 00:06:38 +0330

    Register Cart and CartItem models in Django admin

---
+++ apps/cart/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "session_id", "created_at", "updated_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username", "session_id")
+    readonly_fields = ("created_at", "updated_at", "metadata")
+    ordering = ("-created_at",)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("cart", "variant", "quantity", "created_at", "updated_at")
+    list_filter = ("cart__user", "created_at")
+    search_fields = ("variant__product__name", "variant__name")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("-created_at",)
